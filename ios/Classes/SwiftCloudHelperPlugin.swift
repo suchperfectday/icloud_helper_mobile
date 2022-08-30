3c26bac5eb21b323cc6057d7a4643218ff1289c8
Evgeny Razgovorov <er@pmobi.tech>
2022-08-30 10:45:07 +0700

    Add method fetchLastChanges and delete record

--- ios/Classes/SwiftCloudHelperPlugin.swift
+++ ios/Classes/SwiftCloudHelperPlugin.swift
@@ -3,6 +3,7 @@ import UIKit
 import CloudKit
 
 
+@available(iOS 15.0, *)
 public class SwiftCloudHelperPlugin: NSObject, FlutterPlugin {
     
     private(set) var seedsName: [String] = []
@@ -36,12 +37,39 @@ public class SwiftCloudHelperPlugin: NSObject, FlutterPlugin {
              initialize(call, result)
            case "upload":
              upload(call, result)
+           case "delete":
+             delete(call, result)
            default:
              result(FlutterMethodNotImplemented)
          }
    }
+    
+    func fetchLatestChanges() async throws {
+        var awaitingChanges = true
 
-   private func initialize(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+        while awaitingChanges {
+            let changes = try await database.recordZoneChanges(inZoneWith: zone.zoneID, since: lastChangeToken)
+
+            let changedRecords = changes.modificationResultsByID.compactMapValues { try? $0.get().record }
+            let deletedRecordIDs = changes.deletions.map { $0.recordID.recordName }
+
+            changedRecords.forEach { id, record in
+                if let seedName = record["name"] as? String {
+                    seeds[id.recordName] = seedName
+                }
+            }
+
+            deletedRecordIDs.forEach { seeds.removeValue(forKey: $0) }
+
+            saveChangeToken(changes.changeToken)
+
+            saveLocalCache()
+
+            awaitingChanges = changes.moreComing
+        }
+    }
+
+    private func initialize(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
          guard let args = call.arguments as? Dictionary<String, Any>,
                let containerId = args["containerId"] as? String
          else {
@@ -50,9 +78,9 @@ public class SwiftCloudHelperPlugin: NSObject, FlutterPlugin {
          }
          self.container = CKContainer(identifier: containerId)
          result(nil)
-   }
+    }
 
-    @available(iOS 13.0.0, *)
+    
     private func upload(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) async throws {
         guard let args = call.arguments as? Dictionary<String, Any>,
               let name = args["name"] as? String,
@@ -70,12 +98,49 @@ public class SwiftCloudHelperPlugin: NSObject, FlutterPlugin {
         newRecord["publicKey"] = publicKey
 
         let savedRecord = try await database.save(newRecord)
-        seeds[savedRecord.recordID.recordName] = name
+        seeds[savedRecord.recordID.recordName] = publicKey
         saveLocalCache()
    }
+    
+   func delete(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) async throws {
+       guard let args = call.arguments as? Dictionary<String, Any>,
+             let publicKey = args["publicKey"] as? String
+       else {
+//           result(argumentError)
+           return
+       }
+           
+        guard let matchingID = seeds.first(where: { _, value in publicKey == publicKey })?.key else {
+            debugPrint("Seed not found on deletion for publicKey: \(publicKey)")
+            throw PrivateSyncError.seedNotFound
+        }
+
+        let recordID = CKRecord.ID(recordName: matchingID, zoneID: zone.zoneID)
+
+        try await database.deleteRecord(withID: recordID)
+
+        seeds.removeValue(forKey: matchingID)
+        saveLocalCache()
+    }
     
     
     private func saveLocalCache() {
         UserDefaults.standard.set(seeds, forKey: "seeds")
+    }
+    
+    private func saveChangeToken(_ token: CKServerChangeToken) {
+        let tokenData = try! NSKeyedArchiver.archivedData(withRootObject: token, requiringSecureCoding: true)
+
+        lastChangeToken = token
+        UserDefaults.standard.set(tokenData, forKey: "lastChangeToken")
+    }
+    
+    
+    
+    
+    
+    
+    enum PrivateSyncError: Error {
+        case seedNotFound
     }
 }
