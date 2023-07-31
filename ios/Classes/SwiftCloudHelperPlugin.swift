c2d260462826faabdeb96069be042ef80477698d
Tony <tinhphan.co@gmail.com>
2023-07-31 14:44:03 +0700

    Fix args["fileFieldName"] as String

--- ios/Classes/SwiftCloudHelperPlugin.swift
+++ ios/Classes/SwiftCloudHelperPlugin.swift
@@ -189,7 +189,7 @@ public class SwiftCloudHelperPlugin: NSObject, FlutterPlugin {
         let recordID = CKRecord.ID(recordName: id)
         database!.fetch(withRecordID: recordID) { record, error in
             if let fetchedRecord = record, error == nil {
-                let fileFieldName = args["fileFieldName"] as? String
+                let fileFieldName = args["fileFieldName"] as String
                 if let asset = fetchedRecord[fileFieldName] as? CKAsset,
                     let assetURL = asset.fileURL {
                     result(assetURL.absoluteString)
